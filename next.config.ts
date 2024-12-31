9cb145321c847305b03b26ec382c952ee418db1c
YannisFouzi <yfouzi.dev@gmail.com>
2024-12-31 10:09:24 +0100

    Update Next.js configuration to enhance performance and security
    - Enabled SWC minification for improved build performance.
    - Activated React strict mode to help identify potential issues in the application.
    - Disabled the powered by header for enhanced security and privacy.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  swcMinify: true,
+  reactStrictMode: true,
+  poweredByHeader: false,
 };
 
 export default nextConfig;
